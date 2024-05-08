58f4c53f7ae8c72eb4b112c6443a5b2d7dfcae78
najmul islam <najmulislam519@gmail.com>
2024-05-09 02:46:24 +0600

    date function

---
+++ 09_dates.php
@@ -0,0 +1,15 @@
+<?php 
+// Print current timestamp
+echo time() . "<br>";
+// Print current date
+echo date("Y-m-d H:i:s"). "<br>"
+// Print yesterday
+
+// Different format: https://www.php.net/manual/en/function.date.php
+
+// String to timestamp
+
+// Parse date: https://www.php.net/manual/en/function.date-parse.php
+
+// Parse date from format
+// https://www.php.net/manual/en/function.date-parse-from-format.php
