4f25323427e7eb189ebcc4c3f9761ec562caa054
najmul islam <najmulislam519@gmail.com>
2024-05-13 13:35:10 +0600

    how to work include and reuire

---
+++ 11_include_require/01_website_skeleton/about.php
@@ -0,0 +1,8 @@
+<?php include "partials/header.php" ?>
+<?php
+$year = date("Y");
+?>
+
+<h1>About us</h1>
+
+<?php include "partials/footer.php" ?>
---
+++ 11_include_require/01_website_skeleton/partials/footer.php
@@ -0,0 +1,5 @@
+<footer>
+  Copyright &copy; <?php echo $year ?> TheCodeholic</footer>
+</body>
+
+</html>
