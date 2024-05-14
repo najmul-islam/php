efab05ed81ba2006696e70e8f587d12c7b02e0ed
najmul islam <najmulislam519@gmail.com>
2024-05-14 09:34:11 +0600

    oop in php

---
+++ 14_oop/index.php
@@ -0,0 +1,16 @@
+<?php
+require_once "./Person.php";
+require_once "./Student.php";
+
+$p = new Person("Zuran", 28, null);
+
+// echo $p->name . "<br>";
+// echo $p->age . "<br>";
+// echo $p->getSalary() . "<br>";
+// $p->setSalary(100);
+// echo $p->getSalary() . "<br>";
+// echo "<pre>";
+// var_dump($p);
+// echo "</pre>";
+
+$s = new Student("Zura", 28, 1234);
