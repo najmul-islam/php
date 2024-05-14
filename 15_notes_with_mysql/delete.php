fec99ca07b9ccae0e8ac0729bcd941c1f37114f2
najmul islam <najmulislam519@gmail.com>
2024-05-14 14:31:58 +0600

    notes app with mysql

---
+++ 15_notes_with_mysql/delete.php
@@ -0,0 +1,5 @@
+<?php
+
+$connection = require_once "./Connection.php";
+$connection->removeNote($_POST["id"]);
+header("Location: index.php");
---
+++ 15_notes_with_mysql/save.php
@@ -0,0 +1,11 @@
+<?php
+$connection = require_once "./Connection.php";
+
+$id = $_POST["id"] ?? "";
+if ($id) {
+  $connection->updateNote($id, $_POST);
+} else {
+  $connection->addNote($_POST);
+}
+
+header("Location: index.php");
